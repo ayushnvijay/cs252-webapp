8736c290ac6fb86aebc4c3572f664a9a6e46b6ab
Ayush <ayushnvijay@gmail.com>
2016-04-26 03:11:16 -0400

    TWEEETS

--- Geo+AN/app.js
+++ Geo+AN/app.js
@@ -17,10 +17,16 @@ var cfenv = require('cfenv');
 
 // create a new express server
 var Twitter = require('twitter');
-
+//var convert = require('static-maps');
 
 var app = express();
 //setting up express server
+var client = new Twitter({
+  consumer_key: 'NolHz691z4uhw6FhwDIiKcGXz',
+  consumer_secret: 'FiAz1xTAGmav5xdKVPTqzgci0HX5N5YT8QjhW4L9y0mByGDypo',
+  access_token_key: '4732218563-3dX3xfzpHaDxSIZPgVV6AUxQpBjusVI7senf9qD',
+  access_token_secret: 'Ap5eVnwhkKLwyH12A50X43XO71tOZIFA57VeCikuo6Fln'
+});
 
 app.configure(function(){
     app.set('port', process.env.PORT || 3000);
@@ -46,19 +52,30 @@ app.all('/', function (req, res) {
 
     res.render('index.html');
 });
+app.post('/location', function(req, res){
+    var address = req.body.address;
+	var city = req.body.city;
+	var state = req.body.state;
+    console.log(address);
+    console.log(city);
+    console.log(state);
+    if (!address && !city && !state)
+		res.render('index.html');
+    else{
+    var params = {q: city.toString()};
+        client.get('search/tweets', params, function(error, tweets, response){
+            if (!error) {
+                console.log(tweets);
+            }
+            else{
+                console.log(error);
+            }
+        });
+    }
+});
 
-var client = new Twitter({
-  consumer_key: 'NolHz691z4uhw6FhwDIiKcGXz',
-  consumer_secret: 'FiAz1xTAGmav5xdKVPTqzgci0HX5N5YT8QjhW4L9y0mByGDypo',
-  access_token_key: '4732218563-3dX3xfzpHaDxSIZPgVV6AUxQpBjusVI7senf9qD',
-  access_token_secret: 'Ap5eVnwhkKLwyH12A50X43XO71tOZIFA57VeCikuo6Fln'
-});
-var params = {screen_name: 'ayushnvijay'};
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-  if (!error) {
-    console.log(tweets);
-  }
-});
+
+
 
 
 
@@ -72,7 +89,7 @@ var appEnv = cfenv.getAppEnv();
 
 // start server on the specified port and binding host
 app.listen(appEnv.port, '0.0.0.0', function() {
-
+//convert();
 	// print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
